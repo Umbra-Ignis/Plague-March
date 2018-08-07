3bcd7f9cf2b06b189acfcaebfd38fe85dd6e67f1
s171844 <joel.goodchild@hotmail.com>
2018-08-07 16:21:11 +1000

    UI Press to open image

---
+++ Plague March/Assets/Scripts/UIController_Joel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController_Joel : MonoBehaviour
+{
+    public Image img;
+
+	// Use this for initialization
+	void Start ()
+    {
+        img.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log("UPDATE");
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            Debug.Log("Enabled");
+            if (img.enabled == true)
+                img.enabled = false;
+            else
+                img.enabled = true;
+        }
+	}
+}
