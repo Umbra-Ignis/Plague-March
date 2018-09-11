934a37ac9ab1f1595310208d04c61b75de207af0
Umbra-Ignis <devon.j.sanders@gmail.com>
2018-09-11 12:09:35 +1000

    Updated MainMenu script

--- Plague March/Assets/Scripts/MainMenu.cs
+++ Plague March/Assets/Scripts/MainMenu.cs
@@ -1,14 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; 
+
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject returnMenu;
+    public GameObject Player;
+
+    void Start()
+    {
+        returnMenu.SetActive (false);
+        Time.timeScale = 1f;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-
     }
     public void QuitGame()
     {
@@ -19,4 +29,14 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        returnMenu.SetActive(true);
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        Time.timeScale = 0f;
+
+    }
 }
+
