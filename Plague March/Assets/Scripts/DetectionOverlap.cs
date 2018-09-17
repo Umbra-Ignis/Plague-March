e9d0d438b089918b06d4e42829543f0a837d7e9a
s171844 <joel.goodchild@hotmail.com>
2018-09-17 16:46:57 +1000

    Added overlap sphere class in preperation of altering AI scripts to use raycasts instead of a cone

---
+++ Plague March/Assets/Scripts/DetectionOverlap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionOverlap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Collider[] collider = Physics.OverlapSphere(transform.position, 10);
+
+        DebugExtension.DebugWireSphere(transform.position, 10, 10);
+    }
+}
