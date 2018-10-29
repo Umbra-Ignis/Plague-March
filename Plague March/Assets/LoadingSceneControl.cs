23dcae382e1b432021b70ba34b3c877575d1fa7e
AIE\s171699 <adrianpeds15@gmail.com>
2018-10-29 12:23:46 +1100

    Loading Screen Added

---
+++ Plague March/Assets/LoadingSceneControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneControl : MonoBehaviour {
+
+    
+    public GameObject LoadingScreenPanel;
+    AsyncOperation async;
+
+    public void LoadScreenStart(int Level)
+    {
+        StartCoroutine(LoadingScreen(Level));
+    }
+
+    IEnumerator LoadingScreen(int Level)
+    {
+        LoadingScreenPanel.SetActive(true);
+        async = SceneManager.LoadSceneAsync(Level);
+        async.allowSceneActivation = false;
+
+        while(async.isDone == false)
+        {
+            //PLAY LOADING ANIM
+            if (async.progress == 0.9f)
+            {
+                async.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+
+}
