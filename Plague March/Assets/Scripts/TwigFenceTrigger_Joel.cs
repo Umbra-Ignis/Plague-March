2b23ec78a0f9621eb9cf8a2e27db720069410ba8
s171844 <joel.goodchild@hotmail.com>
2018-11-14 10:37:57 +1100

    Added twig fence prefab and code to allow for this

---
+++ Plague March/Assets/Scripts/TwigFenceTrigger_Joel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwigFenceTrigger_Joel : MonoBehaviour
+{
+    public GameObject fenceStand;
+    public GameObject fenceLay;
+    public bool stand2Lay;
+    public bool lay2Stand;
+
+    private bool doneOnce;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(stand2Lay)
+        {
+            fenceStand.SetActive(true);
+            fenceLay.SetActive(false);
+        }
+
+        if(lay2Stand)
+        {
+            fenceStand.SetActive(false);
+            fenceLay.SetActive(true);
+        }
+
+        doneOnce = false;
+    }
+
+    // Update is called once per frame
+    void Update(){}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!doneOnce)
+        {
+            if (other.CompareTag("Player") && stand2Lay)
+            {
+                fenceStand.SetActive(false);
+                fenceLay.SetActive(true);
+            }
+
+            if (other.CompareTag("Player") && lay2Stand)
+            {
+                fenceStand.SetActive(true);
+                fenceLay.SetActive(false);
+            }
+
+            doneOnce = true;
+        }
+    }
+}
