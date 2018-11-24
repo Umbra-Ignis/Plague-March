db7c23770c04a31e897ac738b8b82380e92355e9
Joel Goodchild <joel.goodchild@hotmail.com>
2018-11-24 22:02:19 +1100

    More commenting

--- Plague March/Assets/Scripts/CreditBannerScroll_Joel.cs
+++ Plague March/Assets/Scripts/CreditBannerScroll_Joel.cs
@@ -11,32 +11,43 @@ using UnityEngine.SceneManagement;
 
 public class CreditBannerScroll_Joel : MonoBehaviour
 {
+    //Stores a reference to the image used for the credits
     private Image bannerImg;
+    //Takes in a float as to how much to increase the scrolling speed of the credits
     public float scrollSpeedMultiplier;
+    //Takes a reference to an image to pop up once the credits have finished
     public Image tooltipImage;
 
     // Use this for initialization
     void Start()
     {
+        //Obtains the reference to the credits image
         bannerImg = GetComponent<Image>();
+        //Ensures the tooltip image is turned off
         tooltipImage.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Checks if the banner is below the top of the screen
         if (bannerImg.transform.position.y <= 2000)
         {
+            //If it is, it progresses it up the screen to give the scrolling effect
             bannerImg.transform.Translate(new Vector3(0.0f, 1.0f * scrollSpeedMultiplier, 0.0f));
         }
         
+        //Checks if the banner is above the top of the screen
         if(bannerImg.transform.position.y >= 2000)
         {
+            //If it is, it will turn on the tooltip image, indicating to the player how to move on
             tooltipImage.enabled = true;
         }
 
+        //Checks if the credits have finished, and if the player chooses to continue by pressing space
         if(tooltipImage.enabled == true && Input.GetKeyDown(KeyCode.Space))
         {
+            //Loads back to the first scene in the build index
             SceneManager.LoadScene(0);
         }
     }
